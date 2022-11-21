2ca671dc443c96e664d5c4a1c1a2419f1e45228c
Anisha-Karmacharya <anisha.karmacharya.01@gmail.com>
2022-11-21 18:50:31 +0545

    changed style of the app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import "./App.css";
-import axios from 'axios';
-import { forecast } from './api';
-import Weather from './components/weather';
-import './App.css'
+import axios from "axios";
+import { forecast } from "./api";
+import Weather from "./components/weather";
+import moment from "moment";
+import "./App.css";
 function App() {
-  const [value, setValue] = useState()
-  const [data, setData] = useState()
-  const handleInputChange = e => {
-    setValue(e.target.value)
+  const [value, setValue] = useState();
+  const [data, setData] = useState();
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
   };
-  const handleSearch = e => {
+  const handleSearch = (e) => {
     e.preventDefault();
-    axios.get(forecast(value)).then(response => {
-      setData(response.data) 
-    })
+    axios.get(forecast(value)).then((response) => {
+      setData(response.data);
+    });
   };
   return (
-    <div className="App" id='app'>
-      <h1 className='appTitle'>
-        Weather App
-      </h1>
-      <form onSubmit={handleSearch} className="search__container">
-        <input
-          type="text"
-          value={value}
-          onChange={handleInputChange}
-          placeholder="Search for location"
-          className="search__input"
-        />
-      </form>
-      {data !== undefined ?
-      <Weather weatherData={data}/>:<div></div>
-      }
-      
+    <div className="App" id="app">
+      <div className="header">
+        <div>
+          <h2>Weather Forecast</h2>
+          <p>{moment().format("dddd MMMM Do YYYY")}</p>
+          <p>{moment().format("h:mm A")}</p>
+        </div>
+        <div>
+          <form onSubmit={handleSearch} className="search__container">
+            <input
+              type="text"
+              value={value}
+              onChange={handleInputChange}
+              placeholder="Search for location"
+              className="search__input"
+            />
+          </form>
+        </div>
+      </div>
+
+      {data !== undefined ? <Weather weatherData={data} /> : <div></div>}
     </div>
   );
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,7 +1,7 @@
 import React from "react";
 import moment from "moment/moment";
 import { BsSunFill } from "react-icons/bs";
-import { WiSunset } from "react-icons/wi";
+import { WiHumidity, WiSunset } from "react-icons/wi";
 import { TiWeatherWindy } from "react-icons/ti";
 import { FaTemperatureHigh, FaTemperatureLow } from "react-icons/fa";
 const Weather = ({ weatherData }) => {
@@ -9,17 +9,20 @@ const Weather = ({ weatherData }) => {
     return {
       key: index,
       main: data.weather[0].main,
+      icon: data.weather[0].icon,
       day: new Date(data.dt * 1000)
         .toLocaleString("en-US", {
-          weekday: "long",
           year: "numeric",
           month: "long",
+          weekday: "long",
           day: "numeric",
         })
         .slice(0, 3),
       desc: data.weather[0].description,
       hTemp: data.temp.max,
       lTemp: data.temp.min,
+      humidity: data.humidity,
+      wind: data.speed,
     };
   });
   const icon = weatherData.list[0].weather[0].icon;
@@ -28,88 +31,93 @@ const Weather = ({ weatherData }) => {
       <div className="weatherInformation">
         {/* Country name , date and time */}
         <div className="locationData">
-          <div>
-          <h1>
+          <h1 className="locationName">
             {weatherData.city.name} {weatherData.city.country}
           </h1>
-          <p>{moment().format("LL")}</p>
-          <p>{moment().format("LT")}</p>
-          </div>
-          <div>
-
-          
-          <div className="todayTemp">
-            <div className="tempImg">
-            <img
-              src={`https://openweathermap.org/img/w/${icon}.png`}
-              alt={icon}
-            />
-             <p className="temp">{weatherData.list[0].temp.day}°C</p>
-            </div>
-            <div>
-             
-              <p> {weatherData.list[0].weather[0].main}</p>
-              <p> {weatherData.list[0].weather[0].description}</p>
-            </div>
-
-          </div>
-
+          <p className="temp">{weatherData.list[0].temp.day}°C</p>
         </div>
-        </div>
-
         {/* Sunrise and sunset */}
-        <div className="suninformation">
-          <>
-            <div>
-              <p>
-                <BsSunFill />
-              </p>
-              <p>
-                {new Date(
-                  weatherData.list[0].sunset * 1000
-                ).toLocaleTimeString().slice(0, 4)} AM
-              </p>
+        <h2 style={{marginTop: "1.5em"}}> Today's Highlight</h2>
+        <div className="todayHighlight">
+          <div className="suninformation">
+            <h3>Sunrise and Sunset</h3>
+            <div className="suninfo">
+              <div>
+                <p>
+                  <BsSunFill style={{color: "#FAAA56", fontSize: "28px"}}/>
+                </p>
+                <p>
+                  {new Date(weatherData.list[0].sunset * 1000)
+                    .toLocaleTimeString()
+                    .slice(0, 4)}{" "}
+                  AM
+                </p>
+              </div>
+              <div>
+                <p>
+                  <WiSunset style={{color: "#FAAA56", fontSize: "28px"}}/>
+                </p>
+                <p>
+                  {new Date(weatherData.list[0].sunrise * 1000)
+                    .toLocaleTimeString()
+                    .slice(0, 4)}{" "}
+                  PM
+                </p>
+              </div>
             </div>
-
+          </div>
+          <div className="extraInformation">
             <div>
-              <p>
-                <WiSunset />
-              </p>
-              <p>
-                {new Date(
-                  weatherData.list[0].sunrise * 1000
-                ).toLocaleTimeString().slice(0, 4)} PM
-              </p>
+              <h3>Wind</h3>
+              <p> {weatherData.list[0].speed}</p>
             </div>
-          </>
-
-          <div>
-            <FaTemperatureHigh />
-            <p> {weatherData.list[0].temp.max}°C</p>
+            <div>
+              <h3>Pressure</h3>
+              <p> {weatherData.list[0].pressure}</p>
+            </div>
+            <div>
+              <h3>Humidity</h3>
+              <p> {weatherData.list[0].humidity}</p>
+            </div>
           </div>
-          <div>
-            <FaTemperatureLow /> <p> {weatherData.list[0].temp.min}°C</p>
-          </div>
         </div>
-      </div>
+        <h2 className="week"> Week</h2>
+        <div className="weekDatas">
+          {weekData.map((data) => (
+            <div className="weekData">
+              <div className="weekDay">
+                <p>{data.day}</p>
+                <img
+                  src={`https://openweathermap.org/img/w/${icon}.png`}
+                  alt={icon}
+                />
+              </div>
 
-      <div>
-
-      </div>
-      <div className="extraInformation">
-        <div>
-          <h3>
-            Wind <TiWeatherWindy />
-          </h3>
-          <p> {weatherData.list[0].speed}</p>
-        </div>
-        <div>
-          <h3>Pressure</h3>
-          <p> {weatherData.list[0].pressure}</p>
-        </div>
-        <div>
-          <h3>Humidity</h3>
-          <p> {weatherData.list[0].humidity}</p>
+              <p className="weekDesc">{data.desc}</p>
+              <div className="weekTemp">
+                <div className="wind">
+                  <p>
+                    <WiHumidity />
+                    {data.humidity}%
+                  </p>
+                  <p>
+                    <TiWeatherWindy />
+                    {data.wind}km/h
+                  </p>
+                </div>
+                <div className="highTemp">
+                  <p>
+                    <FaTemperatureHigh />
+                    {data.hTemp}°C
+                  </p>
+                  <p>
+                    <FaTemperatureLow />
+                    {data.lTemp}°C
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
